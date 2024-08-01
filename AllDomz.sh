5e9eab4defc83014f40bd0e716fb31935d9d2f22
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2024-08-01 09:12:08 +0530

    Update AllDomz.sh

--- AllDomz.sh
+++ AllDomz.sh
@@ -2,7 +2,7 @@
 
 # Function to display usage instructions
 usage() {
-    echo "Usage: $0 <Domain Name>"      
+    echo "Usage: $0 <Domain Name>"
     exit 1
 }
 
@@ -14,11 +14,11 @@ elif [ $# -eq 0 ]; then
 fi
 
 # Run the commands concurrently
-crtsh -d $1 | tee 1.txt &
+crtsh -d $1 | tee 1.txt
 assetfinder $1 | tee 2.txt &
 subdom $1 | tee 3.txt &
 subfinder -d $1 | tee 4.txt &
-shodansubgo -d $1 -s lT9OgIZTFollTgxugbEmriGCWWCQGF5k | tee 5.txt &
+shodansubgo -d $1 -s lT9OgIZTFollTgxugbEmriGCWWCQGF5k | tee 5.txt &        
 shodanx subdomain -d $1 -o 6.txt &
 
 # Wait for all background processes to complete
