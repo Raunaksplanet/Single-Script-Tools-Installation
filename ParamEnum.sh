ebac2695bee8434aa4561d327ef2a57b8e7c6574
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2024-08-01 10:53:19 +0530

    Update ParamEnum.sh

--- ParamEnum.sh
+++ ParamEnum.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 <<comment
 "
 This parameter enumeration script includes these tools
@@ -10,50 +11,50 @@ This parameter enumeration script includes these tools
 "
 comment
 
-
 # Function to display usage instructions
 usage() {
-    echo "Usage: $0 <Domain Name>"
+    echo "Usage: $0 <file containing domains>"
     exit 1
 }
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-
+NC='\033[0m'
 
 # Check if the user provided a filename argument
 if [ $# -eq 0 ]; then
     usage
 elif [ "$1" == "-h" ]; then
     usage
-elif [ ! -f "$1" ]; then
-    echo "File '$1' does not exist."
-    exit 1
 fi
 
-
-file_path="$PWD/$1"
+URL="$1"
 
 # Katana
-echo -e "${RED}1. Katana${NC}";
-katana -silent -ps -nc -jc -kf -fx -xhr -aff -jsl -ef woff,css,png,svg,jpg,ico,otf,ttf,woff2,jpeg,gif,svg -u "$file_path" -o ParamFuzz1.txt &
+echo -e "${RED}1. Katana${NC}"
+katana -silent -ps -nc -jc -jsl -c 50 -ef woff,css,png,svg,jpg,ico,otf,ttf,woff2,jpeg,gif,svg -u "$URL" -o ParamFuzz1.txt 
 
 # Waybackurls
-echo -e "${GREEN}2. WayBackUrls${NC}";
-cat "$file_path" | waybackurls > ParamFuzz2.txt &
+echo -e "${GREEN}2. WayBackUrls${NC}"
+cat "$URL" | waybackurls > ParamFuzz2.txt 
 
 # Gau
-echo -e "${RED}3. Gau${NC}";
-cat "$file_path" | gau > ParamFuzz3.txt &
+echo -e "${RED}3. Gau${NC}"
+cat "$URL" | gau > ParamFuzz3.txt 
 
 # GoSpider
-echo -e "${GREEN}4. GoSpider${NC}";
-gospider -a -w -q -c 20 -d 3 -S "$file_path" | ParamFuzz4.txt & 
+echo -e "${GREEN}4. GoSpider${NC}"
+gospider -a -w -q -c 20 -d 3 -s "$URL" > ParamFuzz4.txt 
 
 # GauPlus
-echo -e "${RED}5. GauPlus${NC}";
-echo "$file_path" | gauplus -random-agent -subs -t 20 > ParamFuzz5.txt &
-
+echo -e "${RED}5. GauPlus${NC}"
+echo "$URL" | gauplus -random-agent -subs -t 20 > ParamFuzz5.txt 
 
 # Wait for all background processes to complete
-wait
+wait $katana_pid
+wait $waybackurls_pid
+wait $gau_pid
+wait $gospider_pid
+wait $gauplus_pid
+
+echo -e "${GREEN}All tools have finished running.${NC}"
