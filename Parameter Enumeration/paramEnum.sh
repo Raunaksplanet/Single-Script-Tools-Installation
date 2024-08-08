d958dab9cfda028bbf34bb04949c4fb63df95b0f
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2024-08-08 22:00:28 +0530

    Update and rename ParamEnum.sh to paramEnum.sh

---
+++ Parameter Enumeration/paramEnum.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Function to display usage instructions
+usage() {
+    echo "Usage: $0 <file containing domains>"
+    exit 1
+}
+
+# Check if the user provided a filename argument
+if [ $# -eq 0 ]; then
+    usage
+elif [ "$1" == "-h" ]; then
+    usage
+fi
+
+URL="$1"
+
+katana -silent -ps -nc -jc -jsl -c 50 -ef woff,css,png,svg,jpg,ico,otf,ttf,woff2,jpeg,gif,svg -u "$URL" | anew ParamFuzz1.txt &
+sleep 3
+
+echo "$URL" | waybackurls | anew ParamFuzz2.txt &
+sleep 3
+
+echo "$URL" | gau | anew ParamFuzz3.txt &
+sleep 3
+
+gospider -a -w -q -c 50 -s "$URL" | anew ParamFuzz4.txt &
+
+# Wait for all background processes to complete
+wait
