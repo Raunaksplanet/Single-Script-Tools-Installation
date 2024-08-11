814fe614c58fe8123c6bfe13476cfbb001c253e7
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2024-08-11 12:04:49 +0530

    Update paramEnum.sh

--- Parameter Enumeration/paramEnum.sh
+++ Parameter Enumeration/paramEnum.sh
@@ -16,17 +16,8 @@ fi
 URL="$1"
 
 katana -silent -ps -nc -jc -jsl -c 50 -ef woff,css,png,svg,jpg,ico,otf,ttf,woff2,jpeg,gif,svg -u "$URL" | anew ParamFuzz1.txt &
-sleep 3
-
-while IFS= read -r url; do
-  echo "$url" | waybackurls | anew ParamFuzz2.txt 
-done < "$URL_FILE"
-sleep 3
-
-echo "$URL" | gau | anew ParamFuzz3.txt &
-sleep 3
-
-gospider -a -w -q -c 50 -s "$URL" | anew ParamFuzz4.txt &
+echo "$URL" | gau > anew ParamFuzz3.txt &
+gospider -a -w -q -c 50 -m 3 -s "$URL" | anew ParamFuzz4.txt &
 
 # Wait for all background processes to complete
 wait
