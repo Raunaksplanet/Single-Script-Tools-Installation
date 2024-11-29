6b17fbddbdd5b351f6ee222b3f219de2ddf48157
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2024-11-29 08:23:30 +0530

    Update AllDomz.sh

--- Sub Domain Enumeration/AllDomz.sh
+++ Sub Domain Enumeration/AllDomz.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# crtsh -d <domain> | anew SubList1.txt; subdom <domain> | anew SubList2.txt; shodanx subdomain -d <domain> -o SubList3.txt; subfinder -silent -nc -d <domain> | anew SubList4.txt; assetfinder -subs-only <domain> | anew SubList5.txt; subdominator -nc -d <domain> | anew SubList6.txt 
 # Function to display usage instructions
 usage() {
     echo "Usage: $0 <Domain Name>"
