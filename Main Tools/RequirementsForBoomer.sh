7dc4117d6a83f0b8acb63001c3cce04d137a5278
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2025-02-06 16:20:23 +0530

    Create RequirementsForBoomer.sh

---
+++ Main Tools/RequirementsForBoomer.sh
@@ -0,0 +1,11 @@
+sudo apt install dnsutils prips wget curl jq -y
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
+go install -v github.com/tomnomnom/anew@latest
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install -v github.com/tomnomnom/assetfinder@latest
+go install -v github.com/devploit/crtsh@latest
+go install -v github.com/tomnomnom/waybackurls@latest
+go install -v github.com/hakluke/hakrawler@latest
+go install -v github.com/jaeles-project/gospider@latest
+go install -v github.com/projectdiscovery/katana/cmd/katana@latest
