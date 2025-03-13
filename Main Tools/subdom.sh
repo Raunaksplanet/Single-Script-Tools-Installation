2ee325fe10e23512caed047842a8328470b4537a
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2025-03-13 18:55:15 +0530

    Update subdom.sh

--- Main Tools/subdom.sh
+++ Main Tools/subdom.sh
@@ -22,9 +22,12 @@ getSubdomains(){
     curl -s "https://tls.bufferover.run/dns?q=.$1" | jq -r .Results 2>/dev/null | cut -d ',' -f3 | grep -o "\w.*$1"| sort -u >> tmp.txt &
     curl -s "https://dns.bufferover.run/dns?q=.$1" | jq -r .FDNS_A[] 2>/dev/null | cut -d ',' -f2 | grep -o "\w.*$1" | sort -u >> tmp.txt &
     curl -s "https://jldc.me/anubis/subdomains/$1" | jq -r '.' | cut -d '"' -f2 | cut -d '[' -f1 | cut -d ']' -f1 | grep . | sort -u >> tmp.txt &
+    curl -s "https://api.certspotter.com/v1/issuances?domain=$1&include_subdomains=true&expand=dns_names" | jq -r '.[].dns_names[]' | sort -u >> tmp.txt &
     curl -s "https://app.netlas.io/api/domains/?q=domain:*.$1+AND+NOT+domain:$1&source_type=include" | jq -r '.items[].data.domain' | sort -u >> tmp.txt &
     curl -s "https://www.threatcrowd.org/searchApi/v2/domain/report/?domain=$1" | jq '.subdomains' | cut -d '"' -f2 | cut -d '[' -f1 | cut -d ']' -f1 | grep . | sort -u >> tmp.txt &
+    curl -s "https://www.virustotal.com/vtapi/v2/domain/report?apikey=2d1ed4d97f91c3c18877c02c5d14225e95c2b5dab7c16a524efa0b94cfd1c0a9&domain=$1" | jq -r '.subdomains[]' | sort -u >> tmp.txt &
     curl -s "http://web.archive.org/cdx/search/cdx?url=*.$1/*&output=text&fl=original&collapse=urlkey" | sort | sed -e 's_https*://__' -e "s/\/.*//" -e 's/:.*//' -e 's/^www\.//' | sort -u >> tmp.txt &
+    curl -s -X GET "https://api.securitytrails.com/v1/domain/$1/subdomains" -H "Accept: application/json" -H "APIKEY: OklShxh2JYjVwzxr7xHefSG4wXdlyCE6" | jq -r '.subdomains[] + ".$1"' | sort -u >> tmp.txt &
 
     csrftoken=$(curl -ILs https://dnsdumpster.com | grep csrftoken | cut -d " " -f2 | cut -d "=" -f2 | tr -d ";")
     curl -s --header "Host:dnsdumpster.com" --referer https://dnsdumpster.com --user-agent "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0" --data "csrfmiddlewaretoken=$csrftoken&targetip=$1" --cookie "csrftoken=$csrftoken; _ga=GA1.2.1737013576.1458811829; _gat=1" https://dnsdumpster.com >> dnsdumpster.html
